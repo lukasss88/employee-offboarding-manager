e0b394523bced7b9464e305c735ace71dc876b7c
lukasss88 <lukaszszymanski88.99@gmail.com>
2025-04-29 13:13:22 +0200

    feat: enhance equipment display in employee details and list components with improved handling for empty states

--- src/app/features/employees/components/employee-details/employee-details.component.ts
+++ src/app/features/employees/components/employee-details/employee-details.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, input, output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,6 +7,7 @@ import { RouterLink } from '@angular/router';
 import { Employee, EmployeeOffboardEvent } from '../../../../core/models/employee';
 import { MatDialog } from '@angular/material/dialog';
 import { OffboardingModalComponent } from '../offboarding-modal/offboarding-modal.component';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-employee-details',
@@ -23,13 +24,16 @@ import { OffboardingModalComponent } from '../offboarding-modal/offboarding-moda
 })
 export class EmployeeDetailsComponent {
   readonly dialog = inject(MatDialog);
+  readonly destroyRef = inject(DestroyRef);
   employee = input.required<Employee>();
   offBoardEmployee = output<EmployeeOffboardEvent>();
 
   openModal() {
     const dialogRef = this.dialog.open(OffboardingModalComponent);
 
-    dialogRef.afterClosed().subscribe((result) => {
+    dialogRef.afterClosed()
+    .pipe(takeUntilDestroyed(this.destroyRef))
+    .subscribe((result) => {
       if (result) {
         this.offBoardEmployee.emit({
           id: this.employee().id,

8d4d5f056d87e7c38c989494bb7d11b9904b3a9d
lukasss88 <lukaszszymanski88.99@gmail.com>
2025-04-29 16:51:41 +0200

    feat: update offboarding modal and form components for improved structure and functionality

--- src/app/features/employees/components/offboarding-modal/offboarding-modal.component.ts
+++ src/app/features/employees/components/offboarding-modal/offboarding-modal.component.ts
@@ -1,11 +1,17 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewChild,
+  inject,
+} from '@angular/core';
 import { MatDialogRef, MatDialogModule } from '@angular/material/dialog';
 import { OffboardingFormComponent } from '../offboarding-form/offboarding-form.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-offboarding-modal',
   standalone: true,
-  imports: [MatDialogModule, OffboardingFormComponent],
+  imports: [MatDialogModule, OffboardingFormComponent, MatButtonModule],
   templateUrl: './offboarding-modal.component.html',
   styleUrl: './offboarding-modal.component.sass',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -13,11 +19,19 @@ import { OffboardingFormComponent } from '../offboarding-form/offboarding-form.c
 export class OffboardingModalComponent {
   readonly dialogRef = inject(MatDialogRef<OffboardingModalComponent>);
 
+  @ViewChild(OffboardingFormComponent) formComponent!: OffboardingFormComponent;
+
   onSubmit(formValue: any): void {
     this.dialogRef.close(formValue);
   }
 
   onCancel(): void {
     this.dialogRef.close();
+  }
+
+  submitForm(): void {
+    if (this.formComponent) {
+      this.formComponent.onSubmit();
+    }
   }
 }
